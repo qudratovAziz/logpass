7d6197ae7d18e6dba7ea40dd12b59cafa76c0293
Qudratov Azizbek <qudratovaziz78@gmail.com>
2021-10-11 10:17:05 +0500

    method va propertylarni yozib qoydim

--- main.py
+++ main.py
@@ -1,2 +1,30 @@
 class User:
-    pass
+    def __init__(self, name, login, password, age):
+        self.name = name
+        self.login = login
+        self.password = password
+        self.age = age
+
+    def register(self):
+        
+
+    def login(self):
+        pass
+
+    def log_out(self):
+        pass
+
+    def update_login(self):
+        pass
+
+    def update_password(self):
+        pass
+
+    def user_forgot_login(self):
+        pass
+
+    def user_forgot_password(self):
+        pass
+
+    def delete_acaunt(self):
+        pass
